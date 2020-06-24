2767f6f75bc1036276b6f83a42c7c549958184b5
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2020-06-24 16:16:39 +0200

    Always print energy if available in dresults (#46)

--- source/program_dftd.f90
+++ source/program_dftd.f90
@@ -179,14 +179,14 @@ program dftd
 ! ------------------------------------------------------------------------
 !  Output:
 ! ------------------------------------------------------------------------
-if (set%lenergy.or.set%lgradient.or.set%lhessian) &
-   call generic_header(istdout,'Results',49,10)
-   if (set%lenergy) then
+   if (set%lenergy.or.set%lgradient.or.set%lhessian) &
+      call generic_header(istdout,'Results',49,10)
+   if (allocated(dresults%energy)) then
       write(istdout,'('// &
-      &      '1x,"Edisp  /kcal,au:",f11.4,1x,f12.8)') &
-      &       dresults%energy*autokcal,dresults%energy
+         &      '1x,"Edisp  /kcal,au:",f11.4,1x,f12.8)') &
+         &       dresults%energy*autokcal,dresults%energy
       write(istdout,'(a)')
-   endif
+   end if
    if (set%ltmer.or.(.not.set%lorca.and.set%lenergy)) &
       call out_tmer('.EDISP',dresults%energy)
 
