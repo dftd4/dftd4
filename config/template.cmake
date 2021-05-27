ddafc45d60238e614868fcc791a5f9573f5b3c17
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-05-27 14:38:52 +0200

    Add support for CMake build system

---
+++ config/template.cmake
@@ -0,0 +1,17 @@
+@PACKAGE_INIT@
+
+set("@PROJECT_NAME@_WITH_OpenMP" @WITH_OpenMP@)
+
+if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
+  include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
+
+  include(CMakeFindDependencyMacro)
+
+  if(NOT TARGET "OpenMP::OpenMP_Fortran" AND "@PROJECT_NAME@_WITH_OpenMP")
+    find_dependency("OpenMP")
+  endif()
+
+  if(NOT TARGET "BLAS::BLAS")
+    find_dependency("BLAS")
+  endif()
+endif()
