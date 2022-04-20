d52aa2cdb0cafa0b7312bbb8c44c57053e26690f
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-04-20 21:11:04 +0200

    Update to CODATA 2018

--- config/cmake/Findmctc-lib.cmake
+++ config/cmake/Findmctc-lib.cmake
@@ -17,7 +17,7 @@
 set(_lib "mctc-lib")
 set(_pkg "MCTCLIB")
 set(_url "https://github.com/grimme-lab/mctc-lib")
-set(_revision "v0.2.3")
+set(_revision "v0.3.0")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
