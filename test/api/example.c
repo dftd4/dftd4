add753298f4c0270ec64cfe36779a4dd325a98e4
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2023-01-01 21:29:45 +0100

    Add error testing for C-API (#186)

--- test/api/example.c
+++ test/api/example.c
@@ -13,109 +13,221 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with dftd4.  If not, see <https://www.gnu.org/licenses/>.
-**/
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "dftd4.h"
 
-int main (void)
+static inline void
+show_error(dftd4_error error)
 {
-   int const natoms = 7;
-   int const nat_sq = natoms * natoms;
-   int const nat3 = natoms * 3;
-   int const nat3_sq = nat3 * nat3;
-   int const attyp[7] = {6,6,6,1,1,1,1};
-   double const coord[21] =
-      {0.00000000000000, 0.00000000000000,-1.79755622305860,
-       0.00000000000000, 0.00000000000000, 0.95338756106749,
-       0.00000000000000, 0.00000000000000, 3.22281255790261,
-      -0.96412815539807,-1.66991895015711,-2.53624948351102,
-      -0.96412815539807, 1.66991895015711,-2.53624948351102,
-       1.92825631079613, 0.00000000000000,-2.53624948351102,
-       0.00000000000000, 0.00000000000000, 5.23010455462158};
-   double energy;
-   double sigma[9];
-   double* pair_disp2;
-   double* pair_disp3;
-   double* gradient;
-   double* hessian;
-   double* c6;
+    char message[512];
+    dftd4_get_error(error, message, NULL);
+    printf("[Message] %s\n", message);
+}
 
-   
-   pair_disp2 = (double*) malloc(nat_sq * sizeof(double));
-   pair_disp3 = (double*) malloc(nat_sq * sizeof(double));
-   gradient = (double*) malloc(nat3 * sizeof(double));
-   hessian = (double*) malloc(nat3_sq * sizeof(double));
-   c6 = (double*) malloc(nat_sq * sizeof(double));
+int test_uninitialized_error(void)
+{
+    printf("Start test: uninitialized error\n");
+    dftd4_error error = NULL;
+    return dftd4_check_error(error) ? 0 : 1;
+}
 
-   
-   if (dftd4_get_version() <= 0) {return 1;}
+int test_uninitialized_structure(void)
+{
+    printf("Start test: uninitialized structure\n");
+    dftd4_error error = NULL;
+    dftd4_structure mol = NULL;
 
-   dftd4_error error;
-   dftd4_structure mol;
-   dftd4_model disp;
-   dftd4_param param;
+    error = dftd4_new_error();
 
-   error = dftd4_new_error();
-   if (!error) {return 1;}
+    double xyz[6] = { 0.0 };
+    dftd4_update_structure(error, mol, xyz, NULL);
+    if (!dftd4_check_error(error))
+        goto unexpected;
 
-   mol = dftd4_new_structure(error, natoms, attyp, coord, NULL, NULL, NULL);
-   if (dftd4_check_error(error)) {return 1;};
-   if (!mol) {return 1;}
+    show_error(error);
 
-   disp = dftd4_new_d4_model(error, mol);
-   if (dftd4_check_error(error)) {return 1;}
-   if (!disp) {return 1;}
+    dftd4_delete(error);
+    return 0;
 
-   // C6 coefficients
-   dftd4_get_properties(error, mol, disp, NULL, NULL, c6, NULL);
-   if (dftd4_check_error(error)) {return 1;}
+unexpected:
+    printf("[Fatal] Unexpected pass for unititalized-structure test\n");
+    dftd4_delete(error);
+    return 1;
+}
 
-   // PBE-D4
-   param = dftd4_new_rational_damping(error, 1.0, 0.95948085, 0.0, 0.38574991, 4.80688534, 16.0);
-   if (dftd4_check_error(error)) {return 1;}
-   if (!param) {return 1;}
+int test_example(void)
+{
+    printf("Start test: example\n");
+    int const natoms = 7;
+    int const nat_sq = natoms * natoms;
+    int const nat3 = natoms * 3;
+    int const nat3_sq = nat3 * nat3;
+    int const attyp[7] = { 6, 6, 6, 1, 1, 1, 1 };
+    double const coord[21] = {
+        +0.00000000000000, +0.00000000000000, -1.79755622305860,
+        +0.00000000000000, +0.00000000000000, +0.95338756106749,
+        +0.00000000000000, +0.00000000000000, +3.22281255790261,
+        -0.96412815539807, -1.66991895015711, -2.53624948351102,
+        -0.96412815539807, +1.66991895015711, -2.53624948351102,
+        +1.92825631079613, +0.00000000000000, -2.53624948351102,
+        +0.00000000000000, +0.00000000000000, +5.23010455462158 };
+    double energy;
+    double sigma[9];
+    double* pair_disp2;
+    double* pair_disp3;
+    double* gradient;
+    double* hessian;
+    double* c6;
 
-   dftd4_get_dispersion(error, mol, disp, param, &energy, NULL, NULL);
-   if (dftd4_check_error(error)) {return 1;}
-   dftd4_get_dispersion(error, mol, disp, param, &energy, gradient, sigma);
-   if (dftd4_check_error(error)) {return 1;}
-   dftd4_get_numerical_hessian(error, mol, disp, param, hessian);
-   if (dftd4_check_error(error)) {return 1;}
+    pair_disp2 = (double*)malloc(nat_sq * sizeof(double));
+    pair_disp3 = (double*)malloc(nat_sq * sizeof(double));
+    gradient = (double*)malloc(nat3 * sizeof(double));
+    hessian = (double*)malloc(nat3_sq * sizeof(double));
+    c6 = (double*)malloc(nat_sq * sizeof(double));
 
-   dftd4_get_pairwise_dispersion(error, mol, disp, param, pair_disp2, pair_disp3);
-   if (dftd4_check_error(error)) {return 1;}
-   dftd4_delete(param);
+    if (dftd4_get_version() <= 0) {
+        goto err;
+    }
 
-   // DSD-BLYP-D4-ATM
-   param = dftd4_load_rational_damping(error, "dsdblyp", true);
-   if (dftd4_check_error(error)) {return 1;}
-   if (!param) {return 1;}
+    dftd4_error error;
+    dftd4_structure mol;
+    dftd4_model disp;
+    dftd4_param param;
 
-   dftd4_get_dispersion(error, mol, disp, param, &energy, NULL, NULL);
-   if (dftd4_check_error(error)) {return 1;}
-   dftd4_get_dispersion(error, mol, disp, param, &energy, gradient, sigma);
-   if (dftd4_check_error(error)) {return 1;}
-   dftd4_get_numerical_hessian(error, mol, disp, param, hessian);
-   if (dftd4_check_error(error)) {return 1;}
-   dftd4_delete(param);
+    error = dftd4_new_error();
+    if (!error) {
+        goto err;
+    }
 
-   dftd4_delete(disp);
-   dftd4_delete(mol);
-   dftd4_delete(error);
+    mol = dftd4_new_structure(error, natoms, attyp, coord, NULL, NULL, NULL);
+    if (dftd4_check_error(error)) {
+        goto err;
+    };
+    if (!mol) {
+        goto err;
+    }
 
-   free(pair_disp2);
-   free(pair_disp3);
-   free(gradient);
-   free(hessian);
-   free(c6);
+    disp = dftd4_new_d4_model(error, mol);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+    if (!disp) {
+        goto err;
+    }
 
-   if (param) {return 1;}
-   if (disp) {return 1;}
-   if (mol) {return 1;}
-   if (error) {return 1;}
+    // C6 coefficients
+    dftd4_get_properties(error, mol, disp, NULL, NULL, c6, NULL);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
 
-   return 0;
+    // PBE-D4
+    param = dftd4_new_rational_damping(error, 1.0, 0.95948085, 0.0, 0.38574991, 4.80688534, 16.0);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+    if (!param) {
+        goto err;
+    }
+
+    dftd4_get_dispersion(error, mol, disp, param, &energy, NULL, NULL);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+    dftd4_get_dispersion(error, mol, disp, param, &energy, gradient, sigma);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+    dftd4_get_numerical_hessian(error, mol, disp, param, hessian);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+
+    dftd4_get_pairwise_dispersion(error, mol, disp, param, pair_disp2, pair_disp3);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+    dftd4_delete(param);
+
+    // DSD-BLYP-D4-ATM
+    param = dftd4_load_rational_damping(error, "dsdblyp", true);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+    if (!param) {
+        goto err;
+    }
+
+    dftd4_get_dispersion(error, mol, disp, param, &energy, NULL, NULL);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+    dftd4_get_dispersion(error, mol, disp, param, &energy, gradient, sigma);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+    dftd4_get_numerical_hessian(error, mol, disp, param, hessian);
+    if (dftd4_check_error(error)) {
+        goto err;
+    }
+    dftd4_delete(param);
+
+    dftd4_delete(disp);
+    dftd4_delete(mol);
+    dftd4_delete(error);
+
+    if (param) {
+        goto err;
+    }
+    if (disp) {
+        goto err;
+    }
+    if (mol) {
+        goto err;
+    }
+    if (error) {
+        goto err;
+    }
+
+    free(pair_disp2);
+    free(pair_disp3);
+    free(gradient);
+    free(hessian);
+    free(c6);
+
+    return 0;
+
+err:
+    if (dftd4_check_error(error)) {
+        char message[512];
+        dftd4_get_error(error, message, NULL);
+        printf("[Fatal] %s\n", message);
+    }
+
+    dftd4_delete(param);
+    dftd4_delete(disp);
+    dftd4_delete(mol);
+    dftd4_delete(error);
+
+    free(pair_disp2);
+    free(pair_disp3);
+    free(gradient);
+    free(hessian);
+    free(c6);
+
+    return 1;
+}
+
+int main(void)
+{
+    int stat = 0;
+    stat += test_uninitialized_error();
+    stat += test_uninitialized_structure();
+    stat += test_example();
+
+    return stat == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
