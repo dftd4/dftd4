d090231b6c2ea92062fe7943c9cdb9a14def2e36
marvinfriede <51965259+marvinfriede@users.noreply.github.com>
2023-09-05 17:51:05 +0200

    Fix copy-paste error

--- app/help.f90
+++ app/help.f90
@@ -1,18 +1,18 @@
-! This file is part of s-dftd3.
+! This file is part of dftd4.
 ! SPDX-Identifier: LGPL-3.0-or-later
 !
-! s-dftd3 is free software: you can redistribute it and/or modify it under
-! the terms of the GNU Lesser General Public License as published by
+! dftd4 is free software: you can redistribute it and/or modify it under
+! the terms of the Lesser GNU General Public License as published by
 ! the Free Software Foundation, either version 3 of the License, or
 ! (at your option) any later version.
 !
-! s-dftd3 is distributed in the hope that it will be useful,
+! dftd4 is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-! GNU Lesser General Public License for more details.
+! Lesser GNU General Public License for more details.
 !
-! You should have received a copy of the GNU Lesser General Public License
-! along with s-dftd3.  If not, see <https://www.gnu.org/licenses/>.
+! You should have received a copy of the Lesser GNU General Public License
+! along with dftd4.  If not, see <https://www.gnu.org/licenses/>.
 
 module dftd4_help
    use dftd4, only : get_dftd4_version
