259583806b99f33004e59e5c27d1f5582a3ede1d
DFT-D4 <49320512+dftd4@users.noreply.github.com>
2024-08-08 18:14:36 +0000

    Deploying to gh-pages from @ dftd4/dftd4@047977ca3c2ef4f9b37e00b54ef481d371ba7d81 🚀

---
+++ src/dftd4.f90
@@ -0,0 +1,31 @@
+! This file is part of dftd4.
+! SPDX-Identifier: LGPL-3.0-or-later
+!
+! dftd4 is free software: you can redistribute it and/or modify it under
+! the terms of the Lesser GNU General Public License as published by
+! the Free Software Foundation, either version 3 of the License, or
+! (at your option) any later version.
+!
+! dftd4 is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! Lesser GNU General Public License for more details.
+!
+! You should have received a copy of the Lesser GNU General Public License
+! along with dftd4.  If not, see <https://www.gnu.org/licenses/>.
+
+module dftd4
+   use mctc_io, only : structure_type, new
+   use dftd4_cutoff, only : realspace_cutoff, get_lattice_points
+   use dftd4_disp, only : get_dispersion, get_properties, get_pairwise_dispersion
+   use dftd4_ncoord, only : get_coordination_number
+   use dftd4_numdiff, only : get_dispersion_hessian
+   use dftd4_damping, only : damping_param
+   use dftd4_damping_rational, only : rational_damping_param
+   use dftd4_model, only : d4_model, new_d4_model
+   use dftd4_param, only : get_rational_damping
+   use dftd4_version, only : get_dftd4_version
+   implicit none
+   public
+
+end module dftd4
