51dd43d9fc4d1c0d8e5ced722f45ddd017c2eabc
thfroitzheim <92028749+thfroitzheim@users.noreply.github.com>
2025-03-31 11:14:48 +0200

    Update dependencies

--- config/cmake/Findmctc-lib.cmake
+++ config/cmake/Findmctc-lib.cmake
@@ -16,8 +16,8 @@
 
 set(_lib "mctc-lib")
 set(_pkg "MCTCLIB")
-set(_url "https://github.com/thfroitzheim/mctc-lib")
-set(_rev "error-handling")
+set(_url "https://github.com/grimme-lab/mctc-lib")
+set(_rev "HEAD")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
