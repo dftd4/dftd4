50764b5ab57dd14912a83b4782e3323ac57916c1
thfroitzheim <92028749+thfroitzheim@users.noreply.github.com>
2025-04-08 10:43:14 +0200

    Pin multicharge version 0.4.0

--- config/cmake/Findmulticharge.cmake
+++ config/cmake/Findmulticharge.cmake
@@ -17,7 +17,7 @@
 set(_lib "multicharge")
 set(_pkg "MULTICHARGE")
 set(_url "https://github.com/grimme-lab/multicharge")
-set(_rev "HEAD")
+set(_rev "v0.4.0")
 
 if(NOT DEFINED "${_pkg}_FIND_METHOD")
   if(DEFINED "${PROJECT_NAME}-dependency-method")
