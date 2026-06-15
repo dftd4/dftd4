c084b445abda9d3b81ea8945a954fb2f51aa4f25
Sebastian Ehlert <sehlert@microsoft.com>
2026-06-15 17:02:39 +0200

    Update config export

--- config/template-config.cmake
+++ config/template-config.cmake
@@ -13,8 +13,8 @@ if(DFTD4_WITH_API)
 endif()
 
 if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
-  include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
   include(CMakeFindDependencyMacro)
+  list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
 
   if(NOT TARGET "OpenMP::OpenMP_Fortran" AND DFTD4_WITH_OpenMP)
     find_dependency("OpenMP")
@@ -31,4 +31,7 @@ if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
   if(NOT TARGET "multicharge::multicharge" AND DFTD4_USE_MULTICHARGE)
     find_dependency("multicharge")
   endif()
+
+  list(REMOVE_AT CMAKE_MODULE_PATH -1)
+  include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
 endif()
