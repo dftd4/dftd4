92afda0da19b826a624a013d98011c0fbbcffb1f
Sebastian Ehlert <sehlert@microsoft.com>
2026-06-18 09:43:47 +0200

    Fix readthedocs build

--- python/dftd4/ase.py
+++ python/dftd4/ase.py
@@ -101,10 +101,7 @@ from ase.calculators.calculator import (
     all_changes,
 )
 from ase.calculators.mixing import SumCalculator
-from ase.units import Bohr as _Bohr, Hartree as _Hartree
-
-Bohr = float(_Bohr)
-Hartree = float(_Hartree)
+from ase.units import Bohr, Hartree
 
 from .interface import DampingParam, DispersionModel
 
